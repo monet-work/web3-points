7a13dacf78027112dacd826ccc048af857e009ce
anujkmonetwork <126776426+anujkmonetwork@users.noreply.github.com>
2024-06-14 20:06:06 +0530

    ASSETS

---
+++ assets/js/acecf23e.39175724.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpoints=self.webpackChunkpoints||[]).push([[1903],{1912:s=>{s.exports=JSON.parse('{"blogBasePath":"/web3-points/blog","blogTitle":"Blog"}')}}]);
---
+++ assets/js/bedc8fa4.a80d3cf3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkpoints=self.webpackChunkpoints||[]).push([[9930],{3756:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/web3-points/blog","page":1,"postsPerPage":10,"totalPages":1,"totalCount":4,"blogDescription":"Blog","blogTitle":"Blog"}}')}}]);
